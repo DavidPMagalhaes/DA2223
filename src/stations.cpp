6118315b6feb8a18abe4b101ec7316fab3123eea
biancasmota <bianca.mota.00@gmail.com>
2023-03-15 09:53:33 +0000

    tried adding stations

--- src/stations.cpp
+++ src/stations.cpp
@@ -1,3 +1,64 @@
-//
-// Created by Tomás Silva on 12/03/2023.
-//
+
+
+
+
+#include "stations.h"
+#include "unordered_set"
+#include "FlightControl.h"
+#include <utility>
+#include <vector>
+#include "string"
+#include "fstream"
+#include "StationGraph.h"
+
+using namespace std;
+
+
+Station(string code, string name, string municipality, string township, string line) {
+    this->code = code;
+    this->name = name;
+    this->municipality = municipality);
+    this->township = township;
+    this->line = line;
+};
+
+string Station::getStationCode() {
+    return code;
+}
+string Station::getStationName() {
+    return name;
+}
+
+string Station::getStationMunicipality() {
+    return municipality;
+}
+string Station::getStationTownship() {
+    return township;
+}
+
+string Station::getStationLine() {
+    return line;
+}
+
+
+unordered_set<Station, StationHash> readStations() {
+    string filename = "dataset/Stations.csv";
+    ifstream file(filename);
+    if (!file.is_open()) {
+        cerr << "Failed to open file: " << filename << endl;
+        return {};
+    }
+
+    unordered_set<Station, StationHash> Stations;
+    string line;
+    getline(file,line);
+    while (getline(file, line)) {
+        if (line.empty()) continue;
+        vector<string> data = FlightControl::split(line, ',');
+        Station x = {data[0], data[1], data[2], data[3], data[4], data[5]};
+        Stations.insert(x);
+
+    }
+    return Stations;
+
+}
--- src/stations.h
+++ src/stations.h
@@ -1,8 +1,49 @@
-//
-// Created by Tomás Silva on 12/03/2023.
-//
+#include "string"
+#include "unordered_set"
+using namespace std;
+class StationHash;
+class Station {
 
-#ifndef DA_PROJ1_STATIONS_H
-#define DA_PROJ1_STATIONS_H
+    string code;
+    string name;
+    string municipality;
+    string township;
+    string line;
 
-#endif //DA_PROJ1_STATIONS_H
+
+    //Station(string code);
+    Station(string code, string name, string municipality, string township, string line);
+
+    string getStationCode();
+
+    string getStationName();
+
+    string getStationMunicipality();
+
+    string getStationTownship();
+
+    string getStationLine();
+
+    static std::unordered_set<Station, StationHash> readStations();
+
+    mutable bool visited;
+
+    bool operator==(const Station &other) const {
+        return code == other.code &&
+               name == other.name &&
+               municipality == other.municipality &&
+               township == other.township &&
+               line == other.line;
+    }
+
+
+    class StationHash {
+        std::size_t operator()(const Station &a) const {
+            std::size_t h1 = std::hash<std::string>()(a.code);
+            return h1;
+        }
+    };
+
+public:
+    Station();
+}
